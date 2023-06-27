6e4c71b2b14fb5a9ba62ff4e460db69255955ce6
Matt Benson <matthew.benson.developer@gmail.com>
2023-06-27 12:13:50 -0400

    Add deno path and completions.

--- dot_config/zsh/config.d/functions.zsh
+++ dot_config/zsh/config.d/functions.zsh
@@ -13,6 +13,10 @@ joinByString() {
 
 # Accepts full command to generate command completions file.
 # See example in .zshrc
+#
+# if $ZSH/completions not a directory or if $ZSH/completions/_$1 file does not exist
+# 1. make completions dir (recursive create -p)
+# 2. run all args as a bash command and write output to file $ZSH/completions/_$1
 add_completions () {
   if [[ ! -d "$ZSH/completions" || ! -f "$ZSH/completions/_$1" ]]; then
     mkdir -p $ZSH/completions
