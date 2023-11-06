beb6476c51a492e35ac25b5c31d1818b364e2c69
Matt Benson <matthew.benson.developer@gmail.com>
2023-11-05 21:53:20 -0500

    Add cargo env to .bashrc, setup for pyenv, and git credential helper for gh CLI to github.com.

--- dot_config/zsh/config.d/path.zsh
+++ dot_config/zsh/config.d/path.zsh
@@ -13,3 +13,10 @@ PATH="$HOME/.local/bin:$PATH"
 
 # add CLI paths for cloud providers
 PATH="/usr/local/google-cloud-sdk/bin:$PATH"
+
+if [ -x "$(command -v pyenv)" ]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
+
