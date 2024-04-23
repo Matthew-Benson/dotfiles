c174f21213aac81c6251a0a5da1bad35b2d21aed
Matt Benson <matthew.benson.developer@gmail.com>
2024-04-23 11:45:20 -0400

    add atuin init to fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    atuin init fish | source
 end
 
 if test -x /opt/homebrew/bin/brew
