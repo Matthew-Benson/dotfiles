6c08ba0a05b27fbc3f51e58dfc63b3e05e29d451
Matt Benson <matthew.benson.developer@gmail.com>
2024-11-25 15:34:36 -0500

    Add completions for new cluster CLI tools

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,9 +3,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    atuin init fish | source
+    atuin init fish --disable-up-arrow | source
 end
 
 if test -x /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
+if type -q direnv
+    direnv hook fish | source
 end
