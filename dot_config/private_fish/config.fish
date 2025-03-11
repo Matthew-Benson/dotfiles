0c3aff12a900d060d04b51c0bc1cabba431e8272
Matt Benson <matthew.benson.developer@gmail.com>
2025-03-11 14:24:07 -0400

    jj update and helm + pnpm completions.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,3 +13,18 @@ end
 if type -q direnv
     direnv hook fish | source
 end
+
+if type -q jj
+    COMPLETE=fish jj | source
+end
+
+if type -q zoxide
+    zoxide init fish | source
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/mbenson/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
