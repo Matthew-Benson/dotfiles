3d648a167b71a6a2f5d6c9c4643100398d2b39b6
Matt Benson <matthew.benson.developer@gmail.com>
2026-01-06 11:12:14 -0500

    Remove zsh support.

--- dot_config/zsh/conf.d/aliases.zsh
+++
@@ -1,6 +0,0 @@
-alias ll="ls -lahG" # this is somewhat standard across installs, but some machines don't define 'll'.
-
-# if podman is installed, alias docker=podman
-if command -v podman &> /dev/null; then
-    alias docker="podman"
-fi
